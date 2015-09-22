e6de00d13b32039c951a0121efa9e695c63a5782
Brandon <brandon@tonzof.com>
2015-09-22 14:15:44 -0600

    Using httpful in doRequest. Raw Curl can sometimes be a PITA.
    Adds support for PUT.

--- src/Client.php
+++ src/Client.php
@@ -1,74 +1,85 @@
 <?php
 namespace Acenda;
+//@todo Acenda SDK has an autoloader - implement using the existing autoloader.
+require __DIR__ . '/../vendor/autoload.php';
+use Httpful;
 
-class Client {
+class Client
+{
     private $client_id;
     private $client_secret;
     private $store_url;
     private $token = ['access_token' => '', 'expires_in' => '', 'token_type' => '', 'scope' => ''];
-    private $ch;
-    public $bypass_ssl=false;
+//    private $ch;
+    public $bypass_ssl = false;
 
-    public function __construct($client_id, $client_secret, $store_url,$plugin_name) {
-        $this->client_id=$client_id;
-        $this->client_secret=$client_secret;
-        $this->store_url=$store_url;
-        $this->plugin_name=$plugin_name;
+    public function __construct($client_id, $client_secret, $store_url, $plugin_name)
+    {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+        $this->store_url = $store_url;
+        $this->plugin_name = $plugin_name;
         $this->initConnection();
     }
 
-    public function initConnection() {
-        list($http_code, $http_json_response) = $this->performRequest('/oauth/token', 'POST', array(    'client_id' => $this->client_id,
-                                                                'client_secret' => $this->client_secret, 
-                                                                'grant_type' => 'client_credentials' 
-                                                            )
-                            );
-        $http_response = json_decode($http_json_response,true);
+    public function initConnection()
+    {
+        list($http_code, $http_json_response) = $this->performRequest('/oauth/token', 'POST', array('client_id' => $this->client_id,
+                'client_secret' => $this->client_secret,
+                'grant_type' => 'client_credentials'
+            )
+        );
+        $http_response = json_decode($http_json_response, true);
         switch ($http_code) {
             case 200:
                 $this->token = $http_response;
                 return true;
                 break;
             default:
-                throw new AcendaException($http_code.": ".$http_response['error']." - ".$http_response['error_description']);
-                break;
+                throw new AcendaException($http_code . ": " . $http_response['error'] . " - " . $http_response['error_description']);
         };
     }
-    
-    public function performRequest($route, $type, $data) {
-        $this->ch = curl_init();
+
+    public function performRequest($route, $type, $data)
+    {
+        /*
+         * So, httpful defaults to strict ssl off - so we would really want to invert this logic here.
+         * @todo Talk to Ahmet to see what the use case was for this - and if it needs to remain. I bet it's for local dev testing.
+         */
+        if ($this->bypass_ssl) {
+//            curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+//            curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
         $data_json = is_array($data) ? json_encode($data) : $data;
-
-        if ($type == 'GET') {
-            $url = $this->store_url.(!empty($this->token['access_token']) ? "/api".$route."?access_token=".$this->token['access_token'] : $route )."&query=".$data_json;
-        }else if($type == 'POST') {
-            $url = $this->store_url.(!empty($this->token['access_token']) ? "/api".$route."?access_token=".$this->token['access_token'] : $route ); 
-            curl_setopt($this->ch, CURLOPT_POST, true);
-            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $data_json);
-            curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(                                                                          
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_json))
-            );
+        $url = $this->store_url . (!empty($this->token['access_token']) ? "/api" . $route . "?access_token=" . $this->token['access_token'] : $route);
+        switch (strtoupper($type)) {
+            case 'GET':
+                //Append the query.
+                $url .= "&query=" . $data_json;
+                $response = Httpful\Request::get($url)->send();
+                break;
+            case 'PUT':
+                $response = Httpful\Request::put($url, $data_json)->sendsJson()->send();
+                break;
+            case 'POST':
+                $response = Httpful\Request::post($url, $data_json)->sendsJson()->send();
+                break;
+            default:
+                throw new AcendaException('Verb ' . $type . ' Not Understood');
         }
 
-        curl_setopt($this->ch, CURLOPT_URL, $url);
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-        if($this->bypass_ssl){
-            curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-        }
-
-        $http_response = curl_exec($this->ch);
-        $http_code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
-        if (curl_errno($this->ch)) { 
-            $http_code = "400";
-            $curl_error['error'] = curl_errno($this->ch);
-            $curl_error['error_description'] = curl_error($this->ch); 
+//        print_r($response);
+        if ($response->code != 200) {
+            $http_code = 400;
+            //There be an error!
+            $curl_error['error'] = $response->body->error;
+            $curl_error['error_description'] = $response->body->error_description;
             $http_response = json_encode($curl_error);
-        } 
-        curl_close($this->ch);
+        } else {
+            //Then it worked!
+            $http_response = $response->raw_body;
+            $http_code = $response->code;
+        }
         return array($http_code, $http_response);
     }
 }
