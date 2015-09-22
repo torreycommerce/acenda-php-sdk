fa443a684b8112d287a2e45778580a800aaffe93
Brandon <brandon@tonzof.com>
2015-09-22 15:27:57 -0600

    Cleanup. Doc Blocks for all methods. Comments.

--- src/Client.php
+++ src/Client.php
@@ -1,5 +1,6 @@
 <?php
 namespace Acenda;
+
 use Httpful;
 
 class Client
@@ -11,6 +12,13 @@ class Client
 //    private $ch;
     public $bypass_ssl = false;
 
+    /**
+     * @param $client_id
+     * @param $client_secret
+     * @param $store_url
+     * @param $plugin_name
+     * @throws AcendaException
+     */
     public function __construct($client_id, $client_secret, $store_url, $plugin_name)
     {
         $this->client_id = $client_id;
@@ -20,6 +28,10 @@ class Client
         $this->initConnection();
     }
 
+    /**
+     * @return bool
+     * @throws AcendaException
+     */
     public function initConnection()
     {
         list($http_code, $http_json_response) = $this->performRequest('/oauth/token', 'POST', array('client_id' => $this->client_id,
@@ -38,6 +50,14 @@ class Client
         };
     }
 
+    /**
+     * @param $route
+     * @param $type
+     * @param $data
+     * @return array
+     * @throws AcendaException
+     * @throws Httpful\Exception\ConnectionErrorException
+     */
     public function performRequest($route, $type, $data)
     {
         /*
@@ -69,18 +89,20 @@ class Client
 //        print_r($response);
         if ($response->code != 200) {
             //This is to catch a blank code.
-            $http_code = $response->code?$response->code:400;
+            $http_code = $response->code ? $response->code : 400;
             //There be an error!
             $curl_error['error'] = $response->body->status;
             $curl_error['error_description'] = $response->body->error;
             $http_response = json_encode($curl_error);
         } else {
-            //Then it worked!
+            //Then it worked! We aren't using any of the httpful response magicalness, so return it raw.
+            /*
+             * If someone is interested in said magicalness, it can basically type juggle the response into a stdClass,
+             * So if you have JSON coming back - it comes back json_decoded. If you have XML, it also comes back as stdClass.
+             */
             $http_response = $response->raw_body;
             $http_code = $response->code;
         }
         return array($http_code, $http_response);
     }
 }
-
-?>
